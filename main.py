7c200114903257b8fffaec3a3a9f4688ba4d1c07
NaleliRD <refiloehape.naleli@gmail.com>
2026-02-09 16:29:55 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ import os
 
 app = FastAPI()
 
-# Absolute pathing ensures Vercel doesn't get "Lost"
 base_path = os.path.dirname(os.path.abspath(__file__))
 
-# Link the image folder
 static_path = os.path.join(base_path, "static")
 app.mount("/static", StaticFiles(directory=static_path), name="static")
 
@@ -18,15 +16,23 @@ quotes_data = [
     {"text": "How do I let our love die, When you're the only other keeper Of my most precious memories?", "author": "I'll Change for You"},
     {"text": "One morning, this sadness will fossilize and I will forget how to cry", "author": "Fireworks"},
     {"text": "As I got older, I learned I'm a drinker Sometimes, a drink feels like family", "author": "Bug Like an Angel"},
-    {"text": "I need something bigger than the sky, Hold it in my arms and know it's mine", "author": "Remember My Name"}
+    {"text": "I need something bigger than the sky, Hold it in my arms and know it's mine", "author": "Remember My Name"},
+    {"text"; "I'm not doing anything, My body's made of crushed little stars, And I'm not doing anything", "author": "My Body’s Made of Crushed Little Stars"},
+    {"text": "Somebody kiss me, I'm going crazy,I'm walking 'round the house naked silver in the night", "author": "Blue Light"},
+    {"text": "If I could, I'd be your little spoon and kiss your fingers forevermore", "author": "Your Best American Girl"},
+    {"text": "And I don't blame you if you want to, Bury me in your memory, I'm not the girl I ought to be", "author": "Goodbye, My Danish Sweetheart"},
+    {"text": "I don't need the world to see that I've been the best I can be, but I don't think I could stand to be where you don't see me", "author": "Francis Forever"},
+    {"text": "I always wanted to die clean and pretty, But I'd be too busy on working days", "author": "Last Words of a Shooting Star"},
 ]
 
-# List of background images (Update this as you add all 10)
+
 background_images = [
     "background.jpg",
     "background2.jpg",
     "background3.jpg",
-    # Add the rest here...
+    "background4.jpg",
+    "background5.jpg",
+    "background6.jpg",
 ]
 
 @app.get("/", response_class=HTMLResponse)
@@ -37,11 +43,9 @@ async def read_index():
 
 @app.get("/get-quote")
 async def get_quote():
-    # Pick a random quote and a random image
     quote = random.choice(quotes_data)
     bg_image = random.choice(background_images)
     
-    # Return both as a single JSON object
     return {
         "text": quote["text"],
         "author": quote["author"],
